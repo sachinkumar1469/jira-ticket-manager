886080602c79b9466fe5089b5d6af29bbf6d1e48
Sachin Kumar <91049993+sachinkumar1469@users.noreply.github.com>
2022-10-30 22:57:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ let modale = document.querySelector('.modale-container');
 let modaleTextarea = document.querySelector('#modaleTextarea');
 let modaleFlag = false;
 
+let colors = ["lightpink","lightblue","lightgreen","black"];
+let modaleCurrColor = colors[colors.length-1];
 
+let modaleColors = document.querySelectorAll('.modaleColors');
+
+// Add btn event listener to open or close modale
 addBtn.addEventListener('click',(e)=>{
-    // Create Modale
     modaleFlag = !modaleFlag;
     modaleHandler();
-    
-
-    // Generate Ticket
+  
 })
 
-function modaleHandler(){
-    
-    console.log(modaleFlag);
+// Function to handle modale
+function modaleHandler(){  
+    // console.log(modaleFlag);
     if(modaleFlag){
         modale.style.display = "flex"; // modaleFlag = true Display the modale
         addBtn.style.backgroundColor = "#718093";
@@ -28,11 +30,12 @@ function modaleHandler(){
 }
 
 
+// Shift event listener on modale to create new ticket
 modale.addEventListener('keydown',(e)=>{
     let key = e.key;
     
     if(key === "Shift"){
-        createTicket();
+        createTicket(modaleCurrColor,"1234",modaleTextarea.value);
         
         modaleFlag = false;
         modaleHandler();
@@ -40,14 +43,28 @@ modale.addEventListener('keydown',(e)=>{
     }
 })
 
-function createTicket(){
+// Function to create a new ticket
+function createTicket(priorityColor,ticketId,ticketContent){
     let ticketContainer = document.createElement('div');
     ticketContainer.setAttribute('class','ticket-container');
-    ticketContainer.innerHTML = `<div class="ticket-priority lightpink"></div>
-    <div class="ticket-id">#1dc456vg</div>
-    <div class="ticket-content">Hello </div>`;
+    ticketContainer.innerHTML = `<div class="ticket-priority ${priorityColor}"></div>
+    <div class="ticket-id">${ticketId}</div>
+    <div class="ticket-content">${ticketContent}</div>`;
     document.querySelector('.main').appendChild(ticketContainer);
 }
+
+//Event listener to select priority color for modale
+modaleColors.forEach((modaleColor,index)=>{
+    modaleColor.addEventListener('click',(e)=>{
+        let previousSelected = colors.indexOf(modaleCurrColor);
+        modaleColors[previousSelected].classList.remove("borderActive");
+        console.log(modaleColors[previousSelected].classList)
+        modaleColor.classList.add("borderActive");
+        modaleCurrColor = colors[index];
+    })
+})
+
+
 
 
 
